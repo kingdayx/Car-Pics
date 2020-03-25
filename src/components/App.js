39c799840afa4b686c97ffedc4899bab2c8eb131
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-03-25 19:31:37 -0400

    made state for searchbar and a function
    made a state for the searchbar to handle what is inputed in the search bar field by the user and ovveriding the value of the search bar field by what is inputed in the search bar by the user. made a prop for search bar in app.js to console log what is input by the user

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css'
 import SearchBar from './SearchBar';
 
-const App = props =>{
+class App extends React.Component{
+   onSearchSubmit(term){
+        console.log(term);
+   }
+   
+    render(){
     return (
         <div className="ui container" id="searchbar" >
-            <SearchBar/>
+            <SearchBar onSubmit={this.onSearchSubmit} />
         </div>
     );
-};
+}};
 
 export default App;
