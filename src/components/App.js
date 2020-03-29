f610b23c249a2ea0e49071fb7c5e48f1bf17abf7
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-03-28 21:31:09 -0400

    created image list to show images upon search

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css'
-import unsplas from '../api/unsplash';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component{
   state = {images: [] };
   
      onSearchSubmit= async (term) =>{
-     const response  = await unsplashs.get('/search/photos',{
+     const response  = await unsplash.get('/search/photos',{
        params:{query: term} 
        });
 
@@ -18,7 +19,7 @@ class App extends React.Component{
     return (
         <div className="ui container" id="searchbar" >
             <SearchBar onSubmit={this.onSearchSubmit} />
-            Found:{this.state.images.length} images
+           <ImageList images={this.state.images} />
         </div>
     );
 }};
